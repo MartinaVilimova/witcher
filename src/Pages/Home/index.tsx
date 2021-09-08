dc74b527b485a660efac70166f02878732e9bc1f
Martina Vilimova <vilima.tul@gmail.com>
2021-09-08 20:18:49 +0200

    Links on the home page

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import picture from '../../assets/home-picture.png'
 import './styles.css'
 
@@ -6,12 +7,22 @@ const Home: React.FC = () => {
     return (
         <div className="wrapper">
             <main className="content">
-                <div className="yrden"></div>
-                <div className="ard"></div>
-                <div className="axie"></div>
-                <div className="quen"></div>
-                <div className="igni"></div>
                 <img className="main-picture" src={picture} alt="witcher" />
+                <Link to="/characters" className="yrden">
+                    <div className="hidden-text">Postavy</div>
+                </Link>
+                <Link to="/creatures" className="ard">
+                    <div className="hidden-text">Monstra</div>
+                </Link>
+                <Link to="/quests" className="axie">
+                    <div className="hidden-text">Úkoly</div>
+                </Link>
+                <a href="https://witcher.fandom.com/wiki/The_Witcher_3:_Wild_Hunt" className="quen">
+                    <div className="hidden-text">TW3 Wiki</div>
+                </a>
+                <a href='https://martina-vilimova-portfolio.web.app/' className="igni">
+                    <div className="hidden-text">Autor</div>
+                </a>
             </main>
         </div>
     )
--- src/features/App/index.tsx
+++ src/features/App/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router';
-import './styles.css';
+import './Styles.css';
 
 // Pages
 import Home from '../../pages/Home';
