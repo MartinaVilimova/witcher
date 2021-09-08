e78d771db36231b81c9ec0cc7150431cc71ce782
Martina Vilimova <vilima.tul@gmail.com>
2021-09-08 13:58:36 +0200

    Insert pictures

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import picture from '../../assets/home-picture.png'
+import './styles.css'
 
 const Home: React.FC = () => {
     return (
-        <div>
-            Witcher
+        <div className="wrapper">
+            <main className="content">
+                <div className="yrden"></div>
+                <div className="ard"></div>
+                <div className="axie"></div>
+                <div className="quen"></div>
+                <div className="igni"></div>
+                <img className="main-picture" src={picture} alt="witcher" />
+            </main>
         </div>
     )
 }
