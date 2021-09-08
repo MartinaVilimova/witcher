ea777510458f74d8620cafb4a6a1e7c5bad76e40
Martina Vilimova <vilima.tul@gmail.com>
2021-09-08 13:58:36 +0200

    Insert pictures

--- src/features/App/index.tsx
+++ src/features/App/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Switch, Route } from 'react-router';
-import './Styles.css';
+import './styles.css';
 
 // Pages
-import Home from '../../Pages/Home';
+import Home from '../../pages/Home';
 
 const Routes: React.FC = () => (
 	<Switch>
