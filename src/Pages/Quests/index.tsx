3eb2b5ab41a489f7936f131253722a487ddd36b1
Martina Vilimova <vilima.tul@gmail.com>
2021-09-09 20:08:54 +0200

    Create Header  component

---
+++ src/Pages/Quests/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../../components/header'
+import './styles.css'
+
+const Quests: React.FC = () => {
+    return (
+        <div className="wrapper-quests">
+            <Header title="Qesty" />
+        </div>
+    )
+}
+
+export default Quests
--- src/utils/getUserData.ts
+++ src/utils/getUserData.ts
@@ -1,19 +1,68 @@
 
 export type RandomCharacter = {
+    id: number
+    gender: string
     name: string
     race: string
     profession: string
     nationality: string
+    firstAppearance: string
     image: string
 }
 
 export const urlCharacters = 'http://witcher3api.com/api/characters'
-export const urlCreatures = 'http://witcher3api.com/api/creatures'
-export const urlQuests = 'http://witcher3api.com/api/quests'
 
 export const getCharactersData = async (): Promise<RandomCharacter[]> => {
     try {
         const data = await fetch(urlCharacters)
+        const jsonData = await data.json()
+        return jsonData
+    } catch (error) {
+        console.error(error)
+        return []
+    }
+}
+
+export type RandomCreatures = {
+    id: number
+    name: string
+    class: string
+    tactics: string
+    susceptibility: string
+    immunity: string
+    quest: string
+    image: string
+    occurences: string
+}
+
+export const urlCreatures = 'http://witcher3api.com/api/creatures'
+
+export const getCreaturesData = async (): Promise<RandomCreatures[]> => {
+    try {
+        const data = await fetch(urlCreatures)
+        const jsonData = await data.json()
+        return jsonData
+    } catch (error) {
+        console.error(error)
+        return []
+    }
+}
+
+export type RandomQuests = {
+    id: number
+    name: string
+    type: string
+    region: string
+    level: string
+    characters: string
+    location: string
+}
+
+export const urlQuests = 'http://witcher3api.com/api/quests'
+
+export const getQuestsData = async (): Promise<RandomQuests[]> => {
+    try {
+        const data = await fetch(urlQuests)
         const jsonData = await data.json()
         return jsonData
     } catch (error) {
